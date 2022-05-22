8b7cd8c0ed0baf79d9226e33d7275261eef097da
itfeelsharsh <itfeelsharsh@gmail.com>
2022-05-22 12:31:48 +0530

    heheh

---
+++ pages/blog.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const blog = () => {
+  return (
+    <div>Here are some cool blogs</div>
+  )
+}
+
+export default blog
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -13,9 +14,18 @@ export default function Home() {
 
       <nav className={styles.mainNav} >
         <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Blog</li>
+          <Link href='/'>
+          <a>Home</a>
+          </Link>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+          <Link href="/blog">
+            <a>Blogs</a>
+          </Link>
+          <Link href="/contact">
+            <a>Contact</a>
+          </Link>
         </ul>
       </nav>
 
@@ -30,23 +40,19 @@ export default function Home() {
             <h3>How to learn HTML in 2022?</h3>
             <p>HTML is the body of website! lets learn it.</p>
           </div>
+          <div className="blogItem">
+            <h3>How to learn HTML in 2022?</h3>
+            <p>HTML is the body of website! lets learn it.</p>
+          </div>
+          <div className="blogItem">
+            <h3>How to learn HTML in 2022?</h3>
+            <p>HTML is the body of website! lets learn it.</p>
+          </div>
         </div>
 
 
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
