4eafa7bcd8e271cd9bb11026dce98919274537ff
itfeelsharsh <itfeelsharsh@gmail.com>
2022-05-26 09:53:58 +0530

    lamo

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 const about = () => {
+
+
+  <Head>
+  <title>About Us - FANTASTIC-GIGGLE blogs</title>
+  <meta name="description" content="About The Super Cool Easy To Read Blogs.." />
+  <link rel="icon" href="/favicon.ico" />
+ </Head>
+
+
   return (
-    <div>This is my about component</div>
+    <main className={styles.main}>
+    <h1 className={styles.title}>FANTASTIC-GIGGLE blogs</h1>
+    <div className={styles.description}>
+    <h3 className={styles.center} >Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum itaque laboriosam aliquam cum perferendis pariatur eveniet. Deserunt nemo sit dolore quia minima unde eos! Dolores similique aperiam molestias deserunt commodi, vitae nulla cumque mollitia nihil earum, numquam sunt veritatis libero voluptate. Cupiditate, quos omnis eligendi sequi ipsam libero nulla consectetur.</h3>
+    </div>
+    </main>
   )
 }
 
